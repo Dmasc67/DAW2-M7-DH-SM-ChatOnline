646e7deb87fbb2a31110d986ddda35362d768198
Dmasc67 <sergi.masip2004@gmail.com>
2024-10-28 16:49:37 +0100

    LogIn simple and css

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+<section class="">
+    <form action="" method="post" id="">
+                    <form>
+                            <label for="">Nombre de usuario:</label>
+                            <input type="input" id="user" name="user" class="" /><br><br>
+                            <label for="">Contraseña:</label>
+                            <input type="password" id="" name="password" id="" class="" /><br><br>
+                            <?php if (isset($_GET[''])) {echo " <br> <br> <p style='text-align: center;'>Usuario o contraseña incorrecto.</p>"; } ?>
+                            <?php if (isset($_GET[''])) {echo " <br> <br> <p style='text-align: center;'>No has ingresado el usuario.</p>"; } ?>
+                            <?php if (isset($_GET[''])) {echo " <br> <br> <p style='text-align: center;'>No has ingresado la contrasña.</p>"; } ?>
+                            <input type="submit" class="" name="" value="Iniciar sesión">
+                            <a class="link" href="">
+                                <p>¡Registrate Gratis!</p>
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </form>
+</section>
+</body>
+</html>
